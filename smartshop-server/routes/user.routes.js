a53f11204d02a4e89593edabadb6f25f14db7b19
TuNguyennAnh <khosithuyminh@gmail.com>
2025-03-30 14:41:24 +0700

    Sửa Login

--- smartshop-server/routes/user.routes.js
+++ smartshop-server/routes/user.routes.js
@@ -4,12 +4,13 @@ const User = require('../models/User');
 const verifyAdmin = require('../middleware/verifyAdmin'); // ✅ middleware kiểm tra quyền admin
 
 // Chỉ admin được xem danh sách user
-router.get('/', verifyAdmin, async (req, res) => {
+router.get('/me', verifyToken, async (req, res) => {
   try {
-    const users = await User.find().select('-password'); // Không trả password
-    res.json(users);
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) return res.status(404).json({ msg: "User not found" });
+    res.json(user);
   } catch (err) {
-    res.status(500).json({ message: 'Lỗi server' });
+    res.status(500).json({ msg: "Lỗi server" });
   }
 });
 
