5b4f79926c7284842622c42ec29ccfc280bea124
TuNguyennAnh <khosithuyminh@gmail.com>
2025-04-01 00:43:57 +0700

    sửa lại

--- smartshop-server/routes/auth.routes.js
+++ smartshop-server/routes/auth.routes.js
@@ -5,4 +5,8 @@ const { login, register } = require("../controllers/auth.controller");
 router.post("/login", login);   // ✅ Phải có dòng này
 router.post("/register", register);
 
+router.get("/test", (req, res) => {
+  res.send("Auth route hoạt động!");
+});
+
 module.exports = router;
