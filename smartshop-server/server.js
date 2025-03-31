acec23e985398b8a449e998f8a57b6684d6e973b
TuNguyennAnh <khosithuyminh@gmail.com>
2025-03-31 19:07:10 +0700

    a

--- smartshop-server/server.js
+++ smartshop-server/server.js
@@ -15,7 +15,6 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server chạy tại http://localhost:${PORT}`));
 
 // Middleware
 app.use(cors({
