f8c3fe30cdde5f91dca29879da92d6fd7d239bd4
TuNguyennAnh <khosithuyminh@gmail.com>
2025-03-31 13:35:43 +0700

    sửa lại

--- smartshop-server/controllers/shop.controller.js
+++ smartshop-server/controllers/shop.controller.js
@@ -1,34 +1,15 @@
-// controllers/shop.controller.js
-
 const Shop = require("../models/shop");
-const User = require("../models/User");
 
-// Tạo shop mới (chỉ dùng khi user được tạo)
-const createShopForUser = async (userId, shopName) => {
+exports.createShopForUser = async (userId) => {
   try {
     const shop = new Shop({
-      name: shopName,
       owner: userId,
+      name: `Shop của người dùng ${userId}`,
+      createdAt: new Date(),
     });
+
     await shop.save();
-    return shop;
   } catch (err) {
-    throw new Error("Không thể tạo shop: " + err.message);
+    console.error("Lỗi khi tạo shop:", err);
   }
-};
-
-// Lấy thông tin shop theo user (shop owner hoặc nhân viên)
-const getMyShop = async (req, res) => {
-  try {
-    const shop = await Shop.findOne({ owner: req.user.id });
-    if (!shop) return res.status(404).json({ message: "Không tìm thấy shop" });
-    res.json(shop);
-  } catch (err) {
-    res.status(500).json({ message: "Lỗi server" });
-  }
-};
-
-module.exports = {
-  createShopForUser,
-  getMyShop,
 };
--- smartshop-server/models/shop.js
+++ smartshop-server/models/shop.js
@@ -1,25 +1,25 @@
-// models/Shop.js
-
 const mongoose = require("mongoose");
 
 const shopSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    unique: true,
-    trim: true,
   },
-
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-    required: true, // mỗi shop phải có 1 chủ
+    required: true,
   },
-
+  employees: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    }
+  ],
   createdAt: {
     type: Date,
     default: Date.now,
-  }
+  },
 });
 
 module.exports = mongoose.model("Shop", shopSchema);
